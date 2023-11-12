c00ae40640e949fdec30f34278a02de0e60f60f0
FullPersonDev <martin.aguero84@gmail.com>
2023-11-12 12:30:18 -0600

    started working on script.js, made original code note and kept it, started experimenting and testing different ways I can accomplish the goal.  Added prompt and the 3 confirm popups for the function on event listener. Added my testing section at bottom of script.js as titled My Trials.

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
+/* Temporarly making original code as Note --
+
 // Assignment Code
 
-// Get references to the #genrate element
+// Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
@@ -14,3 +16,20 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+-- Temporarly making original code as Note */
+
+// my trials
+document.querySelector("#generate").addEventListener("click", generatePassword);
+
+function generatePassword() {
+  var passLenght = parseInt(prompt("How many characters would you like your password to be?"));
+  if (isNaN(passLenght) || passLenght <= 0) {
+    alert("Please enter a valid number greater than 0");
+    return;
+  }
+
+  var includeUppercase = confirm("Would you like UPPERCASE letters?");
+  var includeSpecialChars = confirm("Would you like special characters?");
+  var includeNumbers = confirm("Would you like numbers?");
+};
