26d17de6c7e90bdf72ebc56e25aefc1fa4da3125
FullPersonDev <martin.aguero84@gmail.com>
2023-11-12 22:40:56 -0600

    added a variable that will combine all character sets into one string conditional on user input

--- script.js
+++ script.js
@@ -5,29 +5,37 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  //var password = generatePassword();
+  var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  //Here my function will ask user for characters lenght in a prompt.  Also checking if input was a number:
-  var passLenght = parseInt(prompt("How many characters would you like for your password?"))
-  if (isNaN(passLenght) || passLenght <= 0) {
-    alert("Please enter a valid number and greater than 0")
-    return;
+  //Here I put together the function generatePassword:
+  function generatePassword() {
+    //Here my function will ask user for characters lenght in a prompt.  Also checking if input was a number:
+    var passLenght = parseInt(prompt("How many characters would you like for your password?"))
+    if (isNaN(passLenght) || passLenght <= 0) {
+      alert("Please enter a valid number and greater than 0")
+      return;
+    }
+
+    //Here my function will ask user 3 questions in a confirm, and store them as variables:
+    var includeUppercase = confirm("Would you like UPPERCASE letters?");
+    var includeNumbers = confirm("Would you like numbers?");
+    var includeSpecialChars = confirm("Would you like special characters?");
+
+    //Here my function defines the character sets, and stores them in as variables:
+    var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+    var uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var numberChars = "0123456789";
+    var specialChars = "!@#$%^&*";
+
+    //Here my function will combine the character sets based on user preference:
+    var allChars = lowercaseChars;
+    if (includeUppercase) {allChars += uppercaseChars};
+    if (includeNumbers) {allChars += numberChars};
+    if (specialChars) {allChars += specialChars};
   }
 
-  //Here my function will ask user 3 questions in a confirm, and store them as variables:
-  var includeUppercase = confirm("Would you like UPPERCASE letters?");
-  var includeNumbers = confirm("Would you like numbers?");
-  var includeSpecialChars = confirm("Would you like special characters?");
-
-  //Here my function defines the character sets, and stores them in as variables:
-  var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
-  var uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  var numberChars = "0123456789";
-  var specialChars = "!@#$%^&*";
-
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
